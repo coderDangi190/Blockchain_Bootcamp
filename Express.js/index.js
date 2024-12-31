f6b4562da2d32ab2221e5b608fbed417f3de508e
Sandip Dangi <sandangip920@gmail.com>
2024-12-31 16:54:08 -0700

    client intract server

--- Express.js/index.js
+++ Express.js/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const app = express();
 const PORT = 3000;
+/*
 //to create a middleware
 const sandipmiddleware = (req, res, next) => {
   console.log(res);
@@ -13,7 +15,7 @@ app.get("/user", (req, res) => {
   res.send("Hello Sandip.");
 });
 //to access file from folder
-// app.use(express.static(path.join(__dirname, "Demo")));
+app.use(express.static(path.join(__dirname, "Demo")));
 
 app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -21,6 +23,20 @@ app.get("/home", (req, res) => {
 
 app.get("/user/:userName", (req, res) => {
   res.send("Hello" + req.params.userName);
+});
+*/
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello Sandip");
+});
+
+app.post("/user", (req, res) => {
+  const { Name, gmail, password } = req.body;
+  console.log(`${Name}, ${gmail}, ${password}`);
+  res.status(200).json({ message: "SuccessFull Login" });
 });
 
 app.listen(PORT, () => {
--- React.js/Client/src/App.jsx
+++ React.js/Client/src/App.jsx
@@ -2,33 +2,54 @@ import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
+  /*
   const[count, setCount] = useState(0);
   useEffect(()=>{
     console.log("useEffect");
   },[])
+  */
 
   const nameRef = useRef(null);
+  const gmRef = useRef(null);
   const passRef = useRef(null);
-  const inputReader = (e)=>{
+  
+  const sendData = async(e)=>{
     e.preventDefault()
-    const name = nameRef.current.value;
+    const Name = nameRef.current.value;
+    const gmail = gmRef.current.value;
     const password = passRef.current.value;
+    try{
+      const res = await fetch("http://localhost:3000/user",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+          Name,gmail,password
+        }),
+      })
+      const data = await res.json();
+      console.log(data);
+    }
+    catch(error){
+      console.log(error);
+    }
 
-    console.log(name, password);
   }
 
   return(
     <>
-
+{/*     
       <h1>Count:{count}</h1>
       <button onClick={()=>setCount(count+1)}>Increment</button>
       <br /><br />
-      <button onClick={()=>setCount(count-1)}>Decrement</button> 
+      <button onClick={()=>setCount(count-1)}>Decrement</button>  */}
 
       <h1> FIllup this Form</h1>
 
-        <form onSubmit={inputReader} >
+        <form onSubmit={sendData} >
         <label>Name:-<input type='text' ref={nameRef}/></label><br /><br />
+        <label>Gmail:-<input type='text' ref={gmRef}/></label><br /><br />
         <label>Password:-<input type='password' ref={passRef}/></label><br /><br />
         <button type="submit">Submit</button>
       </form>
